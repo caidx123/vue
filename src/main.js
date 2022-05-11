9a075293e1aac388d23f7a08a93ab406821248f6
caidx123 <1938885900@qq.com>
2022-05-11 16:21:16 +0800

    完成了商品列表的开发

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import './plugins/element.js'
 //导入全局样式表
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+
 Vue.config.productionTip = false
 
 import axios from 'axios'
@@ -19,6 +28,23 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.component('tree-table', TreeTable)
+
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date()
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
